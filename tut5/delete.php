62fa367c84044aa3340f18e4be1ab2581ac31fb0
22SOECE11080 <135407619+22SOECE11080@users.noreply.github.com>
2024-02-23 20:57:57 +0530

    commit

--- tut5/delete.php
+++ tut5/delete.php
@@ -2,7 +2,6 @@
 include_once("conn.php");
 $em = $_REQUEST['em'];
 echo $em;
-// $q = "delete from registration where email='$em'";
 $q = " delete from product_master where p_id='$em' ";
 if (mysqli_query($conn, $q)) {
 ?>
